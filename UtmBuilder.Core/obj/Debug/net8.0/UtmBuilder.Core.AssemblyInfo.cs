65599d28c9dcff5ef9ae366aae25d4f1165efa09
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-07-18 17:40:31 -0300

    Urm class

--- UtmBuilder.Core/obj/Debug/net8.0/UtmBuilder.Core.AssemblyInfo.cs
+++ UtmBuilder.Core/obj/Debug/net8.0/UtmBuilder.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UtmBuilder.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d9b5e57247e7631385e6c63c9a90eb95197095")]
 [assembly: System.Reflection.AssemblyProductAttribute("UtmBuilder.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UtmBuilder.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
