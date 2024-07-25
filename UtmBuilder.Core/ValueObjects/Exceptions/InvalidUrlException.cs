96c99b0cc719a1fd891a2febcfa1ee3353f3b830
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-07-25 10:45:53 -0300

    InvalidUrlException

---
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -0,0 +1,7 @@
+﻿namespace UtmBuilder.Core.ValueObjects.Exceptions;
+
+public class InvalidUrlException : Exception { 
+    public InvalidUrlException(string message = "Url Inválida!") 
+        : base(message) {
+    }
+}
