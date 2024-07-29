2755dcc4f50571512ac9c740dfb2768418d359f0
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-07-29 14:42:54 -0300

    ListExtensions method

---
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidCampaignException.cs
@@ -0,0 +1,11 @@
+﻿namespace UtmBuilder.Core.ValueObjects.Exceptions;
+
+public partial class InvalidCampaignException(string message = InvalidCampaignException.ErrorMessage) : Exception(message) {
+
+    private const string ErrorMessage = "Parâmetros inválidos!";
+
+    public static void ThrowIfInvalidCampaign(string url, string message = ErrorMessage) {
+        if (string.IsNullOrEmpty(url))
+            throw new InvalidCampaignException(message);
+    }
+}
