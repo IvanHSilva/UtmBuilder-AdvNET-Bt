649898625f06d713474bb6d1b460c7c9c91515e9
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-08-01 18:33:06 -0300

    UtmBuilder Tests

---
+++ UtmBuilder.Core.Tests/UtmTests.cs
@@ -0,0 +1,38 @@
+﻿using Utm.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests;
+
+[TestClass]
+public class UtmTests
+{
+
+    private const string Result = "https://amazon.com.br/" + "?utm_source=src" + "&utm_medium=med" +
+                                  "&utm_campaign=nme" + "&utm_id=id" + "&utm_term=ter" + "&utm_content=ctn";
+
+    private readonly Url _url = new("https://amazon.com.br/");
+
+    private readonly Campaign _campaign = new("src", "med", "nme", "id", "ter", "ctn");
+
+    [TestMethod]
+    [TestCategory("Utm")]
+    public void ShouldReturnUrlFromUtm() {
+        var utm = new Utm(_url, _campaign);
+
+        Assert.AreEqual(Result, utm.ToString());
+        Assert.AreEqual(Result, (string)utm);
+    }
+
+    [TestMethod]
+    [TestCategory("Utm")]
+    public void ShouldReturnUtmFromUrl() {
+        Utm utm = Result;
+        Assert.AreEqual("https://amazon.com.br/", utm.Url.Address);
+        Assert.AreEqual("src", utm.Campaign.Source);
+        Assert.AreEqual("med", utm.Campaign.Medium);
+        Assert.AreEqual("nme", utm.Campaign.Name);
+        Assert.AreEqual("id", utm.Campaign.Id);
+        Assert.AreEqual("ter", utm.Campaign.Term);
+        Assert.AreEqual("ctn", utm.Campaign.Content);
+    }
+}
---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,25 @@
+﻿using Utm.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class UrlTests {
+
+    private const string InvalidUrl = "URL inválida";
+    private const string ValidUrl = "https://amazon.com.br";
+
+    [TestMethod]
+    [TestCategory("URL")]
+    [ExpectedException(typeof(InvalidUrlException))]
+    public void ShoulReturnExceptionWhenUrlIsInvalid() {
+        new Url(InvalidUrl);
+    }
+
+    [TestMethod]
+    [TestCategory("URL")]
+    public void ShoulNotReturnExceptionWhenUrlIsValid() {
+        new Url(ValidUrl);
+        Assert.IsTrue(true);
+    }
+}
